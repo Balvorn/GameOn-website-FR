d3941304269df2e0f3dc30f698bfda3124aa9cbf
Balvorn <leo-marcou@hotmail.fr>
2024-03-22 17:42:02 +0100

    90% done

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,13 +11,43 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const modalCloseBtn = document.querySelectorAll(".close");
 
-// launch modal event
+// launch modal events
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+modalCloseBtn.forEach((ele) => ele.addEventListener("click", closeModal));
 
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
 }
 
+// close modal form
+function closeModal() {
+  modalbg.style.display = "none";
+}
 
+// validate form
+function validate(e) {
+  e.preventDefault();
+  let form = e.target;
+  for (let field of form.elements) {
+    if (field.checkValidity()) {
+      field.parentElement.setAttribute("data-error-visible", "false")
+    } else {
+      field.parentElement.setAttribute("data-error-visible", "true")
+    }
+  }
+  if (form.checkValidity()) {
+    validForm()
+  }
+}
+
+//
+function validForm() {
+  var modal = document.querySelector(".modal-body")
+  var height = modal.clientHeight
+  document.getElementById("form").style.display = "none"
+  modal.style.height = height + "px"
+  document.querySelector(".thanks").style.display = "flex"
+}
